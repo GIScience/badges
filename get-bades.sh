11102cbc4a71aea4374381fbd1681f3148eb4bfe
Johannes Visintini <johannes.visintini@heigit.org>
2020-08-26 14:42:45 +0200

    use shields.io from now on

---
+++ get-bades.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+BADGES=(experimental active hibernate archive)
+COLORS=(orange       blue   orange    red)
+
+OUTDIR=status
+
+for index in ${!BADGES[*]}; do
+    badge=${BADGES[$index]}
+    color=${COLORS[$index]}
+    wget https://img.shields.io/badge/status-$badge-$color.svg -O $OUTDIR/$badge.svg
+    wget https://raster.shields.io/badge/status-$badge-$color.png -O $OUTDIR/$badge.png
+done
